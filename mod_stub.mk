ec73f493b50850cb5e34f86d8b5261b5e404fbd2
Chatham Abate <chathamabate@gmail.com>
2026-09-13 22:48:02 -0700

    Alright, it might be bed time for ol chatham

--- mod_stub.mk
+++ mod_stub.mk
@@ -1,2 +1,19 @@
 
-# Clangd + docs I think are also needed.
+ifeq ($(MOD_NAME),)
+$(error MOD_NAME is required)
+endif
+
+ifeq ($(_HDRS),)
+$(error _HDRS is required)
+endif
+
+ifeq ($(_SRCS),)
+$(error _SRCS is required)
+endif
+
+ifeq ($(_TEST_SRCS),)
+$(error _TEST_SRCS is required)
+endif
+
+
+
